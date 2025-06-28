30658f19f435d371fa7f651e40032858768aa944
ANUSRUTA DUTTA <anusruta.dutta@gmail.com>
2025-06-28 19:20:03 +0530

    Initial Commit

---
+++ src/main/java/com/anusruta/expensewon/ExpenseWonApplication.java
@@ -0,0 +1,13 @@
+package com.anusruta.expensewon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseWonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseWonApplication.class, args);
+	}
+
+}
